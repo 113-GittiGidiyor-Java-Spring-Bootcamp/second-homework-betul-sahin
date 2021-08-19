1dd10db55c141c27e5a44f8a77b8a30bef467c17
Betül Şahin <betulsahin3387@hotmail.com>
2021-08-19 13:32:21 +0300

    creation of spring boot project and db configurations

---
+++ SchoolManagementSystemDemoV2/src/main/java/com/betulsahin/springbootcamp/schoolmanagementsystemdemov2/SchoolManagementSystemDemoV2Application.java
@@ -0,0 +1,13 @@
+package com.betulsahin.springbootcamp.schoolmanagementsystemdemov2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementSystemDemoV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolManagementSystemDemoV2Application.class, args);
+    }
+
+}
